b85c28336b4eea2f9f5caa56162ab9e4f23996aa
Hussein1998H <94868084+Hussein1998H@users.noreply.github.com>
2023-08-04 16:01:59 +0300

    ADD Currencyapi

---
+++ BankManagementSystem/app/Http/Controllers/CurrencyController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use CurrencyApi\CurrencyApi\CurrencyApiClient;
+use Illuminate\Http\Request;
+
+class CurrencyController extends Controller
+{
+    public function index(){
+
+
+        $currencyapi = new CurrencyApiClient('cur_live_sX0cEf48NHZKCCrqdeZPz2I2bXjePFCoR5DlrpEf');
+        return response()->json($currencyapi->latest());
+    }
+
+    public function convertMony(Request $request){
+
+        $currencyapi = new CurrencyApiClient('cur_live_sX0cEf48NHZKCCrqdeZPz2I2bXjePFCoR5DlrpEf');
+
+        $value=$request->value;
+
+       $test= $fromdata=$currencyapi->latest([
+             'base_currency' => $request->from,
+             'currencies'=> $request->to
+         ]);
+       $forone= $test['data'][$request->to]['value'];
+       $finaldata=$value*$forone;
+
+        return response()->json($finaldata);
+
+
+    }
+}
